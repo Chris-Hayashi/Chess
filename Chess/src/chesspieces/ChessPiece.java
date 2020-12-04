9883a1dcbe111130d306db6ea30a9b464f496860
Alvin <alvincool650@gmail.com>
2020-12-04 14:20:15 -0800

    Added CheckMove and ChessPiece interface

---
+++ Chess/src/chesspieces/ChessPiece.java
@@ -0,0 +1,10 @@
+package chesspieces;
+
+public interface ChessPiece {
+
+	public String getName();
+	public Boolean getColor();
+	public Boolean getDiag();
+	public Boolean getStraight();
+	public int getSq();
+}
